77796fc7756c68357d8d3d63f7ae82594214ddb2
TSE-Than <tse.web.joomla@gmail.com>
2026-02-06 10:24:14 +0700

    update

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -15,7 +15,7 @@ export default async function App() {
       <Image src="/egat calendar-2026-1.png" width={400} height={400} alt="My Picture" style={{ width: "30%", height: "auto" }} />
       <h1 className="text-3xl text-blue-900">รายชื่อผู้ใช้</h1>
       <ul className="list-disc">
-        {users.map((user: any) => (
+        {users.map((user: {id: number; name: string; email: string}) => (
           <li key={user.id}>
             {user.name} {user.email}
           </li>
