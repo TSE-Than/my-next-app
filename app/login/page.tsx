03288d7059f9431c96f9fbc93fd3901529e01fdf
TSE-Than <tse.web.joomla@gmail.com>
2026-02-06 10:14:58 +0700

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,45 @@
+"use client"
 //rfc
 import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 export default function Loginpage() {
+
+  const router = useRouter()
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  //ฟังก์ชันจัดการการล็อคอิน
+  const handleLogin = (e: React.FormEvent) =>{
+    e.preventDefault()
+    if (username === 'admin' && password === '1234'){
+      alert('Login successful')
+      //สั่งไปหน้า counter
+      router.push('/counter') //ไปที่หน้า counter เมื่อล็อคอินสำเร็จ
+
+    }else{
+      alert('Login failed. Please try agian.')
+    }
+  }
+
+  //สั่งไปหน้า counter
+  //router.push('/counter')
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold text-blue-900 mb-4">Login</h1>
       {/*Login Screen*/}
-      <form className="flex flex-col space-y-4 w-80">
+      <form onSubmit={handleLogin} className="flex flex-col space-y-4 w-80">
         <input
+          onChange={(e) => setUsername(e.target.value)}
           type="text"
           placeholder="Username"
           className="p-2 border border-gray-300 rounded-md"
         />
         <input
+           onChange={(e) => setPassword(e.target.value)}
           type="password"
           placeholder="Password"
           className="p-2 border border-gray-300 rounded-md"
--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,17 +1,30 @@
+"use client"
 //rfc
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
-export default function Navbar() {
+
+export default function Navbar(
+  {href, children}: 
+  {href: string, children: React.ReactNode}
+) {
+
+  const pathname = usePathname()
+  const isActive = pathname === href
+
+  console.log("Current pathname:", pathname)
+
   return (
     <header className="flex bg-blue-800 text-amber-50 p-4">
           <h1 className="text-3xl font-bold"><Link href="/">My App</Link></h1>
           <ul className="flex space-x-4 ml-8 mt-2">
-            <li><Link href="/"></Link>Home</li>
-            <li><Link href="/about"></Link>About</li>
-            <li><Link href="/contract"></Link>Contract</li>
-            <li><Link href="/blog"></Link>Blog</li>
-            <li><Link href="/counter"></Link>Counter</li>
-            <li><Link href="/login"></Link>Login</li>
+            <li><Link className={pathname === '/' ? "text-amber-300" : ""} href="/">หน้าแรก</Link></li>
+            <li><Link className={pathname === '/home' ? "text-amber-300" : ""} href="/home">Home</Link></li>
+            <li><Link className={pathname === '/about' ? "text-amber-300" : ""} href="/about">About</Link></li>
+            <li><Link className={pathname === '/contract'? "text-amber-300" : ""} href="/contract">Contract</Link></li>
+            <li><Link className={pathname === '/blog' ? "text-amber-300" : ""} href="/blog">Blog</Link></li>
+            <li><Link className={pathname === '/counter' ? "text-amber-300" : ""} href="/counter">Counter</Link></li>
+            <li><Link className={pathname === '/login' ? "text-amber-300" : ""} href="/login">Login</Link></li>
           </ul>
     </header>
   )
